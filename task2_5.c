0f05d297cfd240b438428d940285e56cd39b7e65
Ainferno <vasily.nichiporenko@ya.ru>
2018-09-26 19:14:14 +0300

    task2_5_some

--- task2_5.c
+++ task2_5.c
@@ -9,8 +9,71 @@ typedef struct Node
     list next;
 } node;
 typedef list Node;
+
+int cmp_str(char*,char*);
+
+void add(list *a, char *b)
+{
+    if(*a == NULL)
+    {
+        *a = (list)malloc(sizeof(node));
+        (*a)->elem = b;
+        (*a)->next = NULL;
+        return;
+    }
+    add(&(*a)->next, b);
+}
+
+
+void show(list a)
+{
+    if(a == NULL)
+        return;
+    printf("%s%c",a->elem, a->next!=NULL ? ' ' : '\n');
+    show(a->next);
+}
+
+list input()
+{
+    list a = NULL;
+    char *str = NULL;
+    char c;
+    int N = 10, i = 0;
+    str = (char*)malloc(N*sizeof(char));
+
+    for(;scanf("%c",&c)==1;i++)
+    {
+        if(i == N)
+        {
+            N*=2;
+            str = (char*)realloc(str, N*sizeof(char));
+        }
+        if(c == ' ' || c == '\n')
+        {
+            add(&a, str);
+            str = (char*)malloc(N*sizeof(char));
+            i = 0;
+        }
+        str[i] = c;
+    }
+    return a;
+}
+
+int cmp_str(char *a, char *b)
+{
+    for(; (*a) == (*b); a++, b++)
+        if((*a) == '\n')
+            return 1;
+    return 0;
+}
+
 int main()
 {
-    
+    list a = NULL;
+    a = input();
+    show(a);
+    rmv(&a, "abc");
+    show(a);
+    //printf("%d\n", cmp_str("a","a"));
     return 0;
 }
