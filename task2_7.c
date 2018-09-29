c11501264b2842fc27a68c05e7684480823ec630
Vasily <vasily.nichiporenko@ya.ru>
2018-09-29 12:45:06 +0300

    last_last

--- task2_7.c
+++ task2_7.c
@@ -16,7 +16,7 @@ int main()
     printf("==>");
     getlex();
     result=expr();
-    if(curlex != '\n') 
+    if(curlex != '\n')
         error();
     printf("\n%lf\n",result);
     return 0;
@@ -42,17 +42,17 @@ double expr()
 {
     double e=add();
     while(curlex == '+' || curlex == '-')
-    { 
+    {
         if(curlex == '+')
         {
-            getlex(); 
-            e+=add();            
+            getlex();
+            e+=add();
         }
         else
         {
             if(curlex == '-')
             {
-                getlex(); 
+                getlex();
                 e-=add();
             }
         }
@@ -63,17 +63,17 @@ double add()
 {
     double a=mult2();
     while(curlex == '*' || curlex == '/')
-    { 
+    {
         if(curlex == '*')
         {
-            getlex(); 
-            a*=mult2();            
+            getlex();
+            a*=mult2();
         }
         else
         {
             if(curlex == '/')
             {
-                getlex(); 
+                getlex();
                 a/=mult2();
             }
         }
@@ -85,20 +85,20 @@ double mult2()//Alternate function that checks for ^
 {
 //    printf("POINT0 cur = %c\n", curlex);
     double a = mult();
-    int trigger = 0;//Spec trigger that is set 1 if the degree is  <0
+//    int trigger = 0;//Spec trigger that is set 1 if the degree is  <0
     while(curlex == '^')
     {
         getlex();
-        if(curlex == '-')//Here we check if degree is <0
-        {
-            trigger = 1;
-            getlex();
-        }
-        if(trigger)
-            a = 1/degr(a,mult());//If degree <0 => a = 1/a^x
-        else
-            a = degr(a,mult());//If degree >=0 => a = a^x
-        trigger = 0;
+//        if(curlex == '-')//Here we check if degree is <0
+//        {
+//            trigger = 1;
+//            getlex();
+//        }
+//        if(trigger)
+//            a = 1/degr(a,mult());//If degree <0 => a = 1/a^x
+//        else
+        a = degr(a,mult2());//If degree >=0 => a = a^x
+//        trigger = 0;
     }
 //    printf("POINT3 cur = %c\n", curlex);
     return a;
@@ -110,15 +110,15 @@ double mult()
     switch(curlex)
     {
         case '0': case '1': case '2': case '3': case '4': case '5':
-        case '6': case '7': case '8': case '9': 
-            m = curlex-'0'; 
+        case '6': case '7': case '8': case '9':
+            m = curlex-'0';
             break;
-          case '(': 
-            getlex(); 
+          case '(':
+            getlex();
             m=expr();
-            if (curlex == ')') 
+            if (curlex == ')')
                 break;
-        default : 
+        default :
             error();
     }
     getlex();
